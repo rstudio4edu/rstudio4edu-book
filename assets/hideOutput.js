85d9a177ba92e2a154240db54385f850c4509814
Desirée De Leon <dcossyleon@gmail.com>
2019-08-01 00:11:53 -0400

    Closes #33, creates anchored sections with link icons for headers

--- assets/hideOutput.js
+++ assets/hideOutput.js
@@ -11,4 +11,10 @@ $(document).ready(function() {
       $temp.remove();
     });
   });
+  
+  // Section anchors
+  $('.section h1, .section h2, .section h3, .section h4, .section h5').each(function() {
+    anchor = '#' + $(this).parent().attr('id');
+    $(this).addClass("hasAnchor").prepend('<a href="' + anchor + '" class="anchor"></a>');
+  });
 });
