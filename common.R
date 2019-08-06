d0d30c258ee2f805b74251c2e3d349d98f1865af
Desirée De Leon <dcossyleon@gmail.com>
2019-08-05 23:44:14 -0400

    Adds screenshot and updates common.R

--- common.R
+++ common.R
@@ -17,3 +17,18 @@ build_toc <- function(filename) {
   headers <- paste(recipes, collapse = "\n")
   cat("<div class='col2'>\n", headers, "\n</div>") 
 }
+
+# for side-by-side images. path must be in quotes, width just a #.
+side_by_side <- function(path1, width1, path2, width2){
+  style2 <- paste0("padding-left: 1rem; width: ", width2, "%;")
+  style1 <- paste0("width: ", width1, "%;")
+  
+  htmltools::withTags(
+    div(style = "display: flex;",
+          div(style = style1, 
+              htmltools::img(src = path1)), 
+          div(style = style2, 
+              htmltools::img(src = path2))
+    )
+  )
+}
